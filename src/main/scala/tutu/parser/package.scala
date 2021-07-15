cce007fa60b4d3c6ce7262bc0bd7cc9900cd818c
Maichi Kuno <kuno.maichi@gmail.com>
2021-07-15 17:13:14 +0900

    Add utility methods for parser combinators

---
+++ src/main/scala/tutu/parser/package.scala
@@ -0,0 +1,27 @@
+package tutu
+import org.javafp.parsecj._
+import org.javafp.parsecj.Combinators._
+import org.javafp.parsecj.input.Input
+
+package object parser {
+  type TutuParser[A] = Parser[Input[Char], A]
+  case class ~[A, B](_1: A, _2: B)
+  implicit class RichParser[A](self: TutuParser[A]) {
+    def flatMap[B](function: A => TutuParser[B]): TutuParser[B] = {
+      self.bind(a => function(a))
+    }
+    def map[B](function: A => B): TutuParser[B] = {
+      self.map(a => function(a))
+    }
+    def |(that: TutuParser[A]): TutuParser[A] = {
+      self.or(that)
+    }
+    def ~[B](that: TutuParser[B]): TutuParser[A ~ B] = {
+      self.bind(a =>
+        that.bind(b =>
+          retn(_root_.tutu.parser.~(a, b))
+        )
+      )
+    }
+  }
+}
