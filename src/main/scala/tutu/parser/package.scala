5b2fd114c5d2ebb880f704d58c5a6b10eef6a541
Maichi Kuno <kuno.maichi@gmail.com>
2021-07-15 18:07:37 +0900

    Parser implementation has just begun

--- src/main/scala/tutu/parser/package.scala
+++ src/main/scala/tutu/parser/package.scala
@@ -4,10 +4,12 @@ import org.javafp.parsecj.Combinators._
 import org.javafp.parsecj.input.Input
 import org.javafp.parsecj.Text
 
+import java.util.Optional
+
 package object parser {
   type TutuParser[A] = Parser[Character, A]
   case class ~[A, B](_1: A, _2: B)
-  implicit class RichParser[A](self: TutuParser[A]) {
+  implicit class RichParser[A >: Null](self: TutuParser[A]) {
     def flatMap[B](function: A => TutuParser[B]): TutuParser[B] = {
       self.bind(a => function(a))
     }
@@ -24,7 +26,13 @@ package object parser {
         )
       )
     }
+    def withFilter(predicate: A => Boolean): TutuParser[A] = {
+      self.flatMap(a => if(predicate(a)) self else fail("condition doesn't satisfy"))
+    }
   }
-  def string(value: String): TutuParser[String] = Text.string(value)
+  def literal(value: String): TutuParser[String] = Text.string(value)
+  def optional[A >: Null](value: TutuParser[A]): TutuParser[Option[A]] = {
+    value.optionalOpt().map[Option[A]]((v: Optional[A]) => Option(v.orElse(null)))
+  }
   def success[A](value: A): TutuParser[A] = retn(value)
 }
